3cbde342f75b3b9b1139058dccbc0911d78cd4f2
Vladislav Burkatsky <feedmemorekurama@gmail.com>
2020-03-04 11:43:05 +0200

    Homework 3 completed

---
+++ homework/hm_3/eq_13.py
@@ -0,0 +1,5 @@
+from math import *
+
+x = int(input("Enter x ="))
+y = ((e**-2+x)**1/5 ) * (1) / ((x**2) + (x**4) + log(abs(x - 3.14))**1/2)
+print(y)
---
+++ homework/hm_3/eq_15.py
@@ -0,0 +1,5 @@
+from math import *
+
+x = int(input("Enter x ="))
+y = (abs(x*log(x-4))*(x**1/2)*((1)/((e**4*x-1)**1/5)))
+print(y) #Получаем результат функции x=5,y=0.0 и выше
