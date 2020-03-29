64fe1ea74d1dab445695346e3c495cdff661ad7a
Vladislav Burkatsky <feedmemorekurama@gmail.com>
2020-03-29 13:28:59 +0300

    Update my homework 6

--- homework/hw_6/divide without remainder.py
+++ homework/hw_6/divide without remainder.py
@@ -3,11 +3,9 @@ new_list = []
 idx = 0
 for i in my_list:
     if idx == 0:
-        idx+=1
+        print("you can't divide by zero")    
     elif i % idx ==0:
         new_list.append(i)
-        idx+=1
-    else:
-        idx+=1
+    idx+=1
 
 print(new_list)
