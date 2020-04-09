116c0c498f27b3e07f8d808812bb2a176526b9e0
Vladislav Burkatsky <feedmemorekurama@gmail.com>
2020-04-10 00:14:51 +0300

    Update homework

--- Homework/hw_8/try_to_guess.py
+++ Homework/hw_8/try_to_guess.py
@@ -1,19 +1,18 @@
 import random
 
-tries = 0
+tries = 6
 number = random.randint(1, 50)
-print(number)
 
 print("Try to guess!")
-while tries < 6:
+while tries > 0:
     attempts = int(input("Enter your num:"))
-
-    tries+=1
+    tries-=1
     if attempts < number:
         print("You guess is too low")
     elif attempts > number:
         print("Your guess is too high")
     elif attempts == number:
-        print("Congrats! You guessed my number!")    
-    elif attempts !=number and tries ==6:
-        print("Sorry, but your didn't make it.")
+        print("Congrats! You guessed my number!")   
+        break 
+else:
+    print("Sorry, but your didn't make it.")
