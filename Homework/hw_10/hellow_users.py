de60e2bc27b172b7020db63558b0b6f8a51e3bc8
Vladislav Burkatsky <feedmemorekurama@gmail.com>
2020-04-11 11:06:56 +0300

    Finally!

--- Homework/hw_10/hellow_users.py
+++ Homework/hw_10/hellow_users.py
@@ -5,15 +5,9 @@ roles = {
     "developer": ['Carlos','Max','Artur']}
 
 name = input("My name is, ")
-for i in roles.items():
-    if name in roles['admin']:
-        print("Hello, admin!")
-    elif name in roles['maintainer']:
-        print("Hello, maintainer!")
-    elif name in roles['manager']:
-        print("Hello, manager!")
-    elif name in roles['developer']:
-        print("Hello, developer!")
-    else:
-        print("Hello, Guess")
-    break
+for role, names in roles.items():
+    if name in names:
+        print(f"Hello, {role}")
+        break
+else:
+    print("Hello, Guess")
